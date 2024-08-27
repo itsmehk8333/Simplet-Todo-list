62eb16d9ce817898e4209b83b10ac12ca983a68c
itsmehk8333 <gunnalaharikrishna@gmail.com>
2024-08-27 12:10:08 +0530

    style chnages

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Homepage from './Components/Homepage';
 import Todo from './Components/Todo';
 
 function App() {
-  return (
+  return ( 
     <div className="App">
-    <h1 className="todo-title">TODO LIST</h1>
+   <div style={{width:"500px" , border:"1px solid #6A7ECE" , padding:"10px 100px", background:"#6A7ECE", marginTop:"50px"}}>
+   <h1 className="todo-title">TODO LIST</h1>
      <Homepage />
      <Todo />
+   </div>
     </div>
   );
 }
--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -4,32 +4,32 @@ import { ADDFunc } from '../Redux/Action';
 import './Hompage.css'
 
 function Homepage() {
-const[inputvalue , SetinputValue] = useState("");
+  const [inputvalue, SetinputValue] = useState("");
 
-const Dispatch = useDispatch();
-const id = Math.floor(Math.random()*1000)
-  return (
-    <div>
-<div className='inputDiv'>
+  const Dispatch = useDispatch();
+  const id = Math.floor(Math.random() * 1000)
+  return (<div>
+    <div className='inputDiv'>
 
-<input type="text" className='Input'  placeholder='  Add Your Todo' onChange={(e) =>{ SetinputValue(e.target.value); }} / >
-    <button
-    onClick={()=>{
-      if(inputvalue ===""){
-        window.alert("please enter something")
-      }else{
-        Dispatch(ADDFunc(
-          {
-        id:id,
-        input: inputvalue,
+      <input type="text" className='Input' placeholder='  Add Your Todo' onChange={(e) => { SetinputValue(e.target.value); }} value={inputvalue} />
+      <button
+        onClick={() => {
+          if (inputvalue === "") {
+            window.alert("please enter something")
+          } else {
+            Dispatch(ADDFunc(
+              {
+                id: id,
+                input: inputvalue,
+              }
+            ))
+            SetinputValue("")
           }
-        ))
-      }
-    }} className="ADD">ADD</button>
- 
-</div>
-    
+        }} className="ADD">ADD</button>
+
     </div>
+
+  </div>
   )
 }
 
